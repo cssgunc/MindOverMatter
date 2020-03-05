fb372858e7d71f9c492d5a4d74812ec7e81930e8
maysonsnavely <maysonsnavely@gmail.com>
2020-03-05 18:38:48 -0500

    help

--- help.py
+++ help.py
@@ -2,6 +2,21 @@ from __main__ import app
 from flask import render_template
 from flask import Flask
 
+
+class Resource:
+    def __init__(self, link, title, description, category):
+        self.link = link
+        self.title = title
+        self.description = description
+        self.category = category
+
+help_list = {
+    Resource("link", "title", "description", "call"), 
+    Resource("https://suicidepreventionlifeline.org/chat/", "Suicide Chatline", "Get in touch with a counselor who will listen to you, understand how your problem is affecting you, provide support and share resources that may be helpful.", "chat_resource_one"),
+    Resource("https://www.nami.org/", "National Alliance on Mental Health Chatline", "Connect with a trained crisis counselor to receive free, 24/7 crisis support via text message.", "chat_resource_two"),
+    Resource("link", "title", "description", "website")
+}
+
 @app.route('/help')
 def get_help():
-    return render_template('GitHelpNow1.html')
+    return render_template('GitHelpNow1.html', help_list=help_list)
