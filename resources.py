9d60ba31cb1740c12f6705b0f9b4be541f764ce2
Adam Winek <adamwinek@gmail.com>
2020-07-06 19:24:39 -0400

    Made search bar like Nicholas asked for

--- resources.py
+++ resources.py
@@ -2,6 +2,8 @@ from __main__ import app
 from flask import render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from database import db, Post
+import re
+
 
 @app.route('/resources/<int:resource>')
 def resourceRoute(resource):
@@ -10,3 +12,17 @@ def resourceRoute(resource):
 @app.route('/resources/')
 def resources():
     return render_template('resources.html', ResourcesList=Post.query.all())
+
+@app.route('/resources/search/<searchField>')
+def resourceSearch(searchField):
+    searchedList = list()
+    for post in Post.query.all():
+        currentPost = re.findall(searchField, post.title + post.description)
+        print(currentPost)
+        if currentPost != []:
+            searchedList.append(post)
+        print(searchedList)
+    
+    return render_template('resources.html', ResourcesList=searchedList)
+
+    
